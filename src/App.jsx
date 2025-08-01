41569225fd0f352f23da9d693dbafcdc8d471424
Alejo Oviedo <alejoemmanueloviedo@gmail.com>
2025-08-01 19:44:27 -0300

    Primera entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import { useState, useEffect } from 'react'
-import NavBar from "./NavBar"
-import Header from './Header'
-import MainBody from "./MainBody"
-import Footer from "./Footer"
-import Button from "./Button"
-import CartWidget from "./CartWidget"
+import { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+import Header from "./Header";
+import MainBody from "./MainBody";
+import Footer from "./Footer";
+import Button from "./Button";
+import CartWidget from "./CartWidget";
 function App() {
-
-  const [showNavBar, setShowNavBar] = useState (false)
+  const [showNavBar, setShowNavBar] = useState(false);
   const [opened, setOpened] = useState(false);
 
   useEffect(() => {
-    document.body.classList.toggle('nav-visible', showNavBar);
+    document.body.classList.toggle("nav-visible", showNavBar);
   }, [showNavBar]);
   return (
     <>
-      <div className={`main-content ${showNavBar ? 'blurred' : ''}`}>
-      <Header />
-      <MainBody/>
-      <Footer />
+      <div className={`main-content ${showNavBar ? "blurred" : ""}`}>
+        <Header />
+        <MainBody />
+        <Footer />
+      </div>
       <CartWidget></CartWidget>
-    </div>
-
-      <Button opened={opened} setOpened={setOpened} setShowNavBar={setShowNavBar} showNavBar={showNavBar}></Button>
-      <NavBar esVisible = {showNavBar}></NavBar>
-  
+      <Button
+        opened={opened}
+        setOpened={setOpened}
+        setShowNavBar={setShowNavBar}
+        showNavBar={showNavBar}
+      ></Button>
+      <NavBar esVisible={showNavBar}></NavBar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
